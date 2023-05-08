ffd941dd9c3d721a76cfdf4c0b746c50bac2632f
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2023-05-08 18:34:34 +0300

    adding questions, correct answer functionality, ux

--- silky/views.py
+++ silky/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponseRedirect, request
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView
 
 from accounts.forms import CustomUserCreationForm
-from silky.forms import TestQuestionForm
 from silky.models import TestQuestion, TestAnswer
 
 
@@ -38,21 +36,18 @@ def question_detail_view(request, pk=8):
     question = TestQuestion.objects.filter(pk=pk).first()
     if request.method == "POST":
         answer = request.POST.get("q.pk")
-        print("-------->", answer)
         data = {"answer": answer, "test_question": question, "author": request.user}
-        print(data)
         new_answer = TestAnswer.objects.create(**data)
         new_answer.save()
-        results = TestAnswer.objects.filter(author=request.user)
         return redirect('results')
     else:
         return render(request, 'question.html', {'q': question})
 
 
 def user_results_view(request):
-    questions = list(TestQuestion.objects.filter(answers__author=request.user))
-    results = [{"question":q.question, "correct": q.correct_answer,"1":q.answer_1, "2":q.answer_2, "3":q.answer_3,
-                "4":q.answer_4,"response":TestAnswer.objects.filter(author=request.user, test_question=q)
-                    .values().first()["answer"]} for q in questions]
-    print(results)
+    questions = list(TestQuestion.objects.filter(answers__author=request.user).order_by("-answers__id"))
+    results = [{"question": q.question, "correct": q.correct_answer, "1": q.answer_1, "2": q.answer_2, "3": q.answer_3,
+                "4": q.answer_4, "response": TestAnswer.objects.filter(author=request.user, test_question=q)
+        .values().first()["answer"], "is_right":TestAnswer.objects.filter(author=request.user, test_question=q)
+                                                 .values().first()["answer"] == q.correct_answer} for q in questions]
     return render(request, 'test_results.html', {'results': results})
